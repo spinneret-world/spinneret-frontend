aed1572d66bb253fcdbcbe9ef70875b9824b17b3
4lp <alptraq@gmail.com>
2020-12-08 15:21:14 -0600

    Initial commit - spin up Vue app and start on Blog

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,34 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import axios from "axios";
 
 Vue.use(Vuex);
 
+const API = "http://localhost:3000";
+
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    posts: []
+  },
+  getters: {
+    posts: state => state.posts
+  },
+  mutations: {
+    setPosts(state, posts) {
+      state.posts = posts;
+    }
+  },
+  actions: {
+    fetchPosts({ commit }) {
+      axios.get(`${API}/posts`).then(
+        response => {
+          commit('setPosts', response.posts);
+        },
+        error => {
+          console.log(error);
+        }
+      );
+    }
+  },
   modules: {}
 });
---
+++ src/views/Blog.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <div>
+      Blawg
+    </div>
+    <div v-for="post in posts" :key="post.id">
+      {{ post }}
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapActions, mapGetters } from 'vuex';
+
+export default {
+  name: "Blog",
+  components: {
+  },
+  data() {
+    return {}
+  },
+  computed: {
+    ...mapGetters(['posts'])
+  },
+  mounted() {
+    this.fetchPosts();
+  },
+  methods: {
+    ...mapActions(['fetchPosts'])
+  }
+};
+</script>
+
+<style lang="scss">
+</style>
